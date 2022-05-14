ae591bf1bb7a7d73d2852fb726a7e760d268f9f8
Andrii Tsuvalovskyi <tsuvalovskyi@gmail.com>
2022-05-14 12:13:35 +0300

    Added assignment 9 and translated assignment 5

--- HomeAssignments/unit4-5/05.php
+++ HomeAssignments/unit4-5/05.php
@@ -2,7 +2,7 @@
 
 $number = random_int(1000, 9999);
 
-echo "Random number: " . $number . "<br>";
+echo "Випадкове число: " . $number . "<br>";
 
 $thousands = ($number - $number % 1000) / 1000;
 $number -= $thousands * 1000;
@@ -12,8 +12,8 @@ $tens = ($number - $number % 10) / 10;
 $number -= $tens * 10;
 $sum = $thousands + $hundreds + $tens + $number;
 
-echo "Thousands: " . $thousands . "<br>";
-echo "Hundreds: " . $hundreds . "<br>";
-echo "Tens: " . $tens . "<br>";
-echo "Last: " . $number . "<br>";
-echo "Sum: " . $sum . "<br>";
+echo "Тисячі: " . $thousands . "<br>";
+echo "Сотні: " . $hundreds . "<br>";
+echo "Десятки: " . $tens . "<br>";
+echo "Остання цифра: " . $number . "<br>";
+echo "Сума: " . $sum . "<br>";
