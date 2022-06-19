d38662d86f20df8a45eb65d1ebe19eab815b55e1
Andrii Tsuvalovskyi <tsuvalovskyi@gmail.com>
2022-06-19 17:27:11 +0300

    Added assignment 20

---
+++ HomeAssignments/unit13/posts.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `hillel`.`posts`
+(
+    `id`      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user`    BIGINT UNSIGNED NOT NULL,
+    `title`   VARCHAR(255) NOT NULL,
+    `content` MEDIUMTEXT   NOT NULL,
+    `image`   VARCHAR(255) NOT NULL,
+    `created` DATETIME     NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
