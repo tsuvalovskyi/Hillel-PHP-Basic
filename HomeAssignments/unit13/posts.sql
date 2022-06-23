d5af5c6f0d75414b278bbae1d2d6f3d3bcdbd5fd
Andrii Tsuvalovskyi <54897493+tsuvalovskyi@users.noreply.github.com>
2022-06-23 19:29:40 +0300

    Update posts.sql

--- HomeAssignments/unit13/posts.sql
+++ HomeAssignments/unit13/posts.sql
@@ -4,7 +4,7 @@ CREATE TABLE `hillel`.`posts`
     `user`    BIGINT UNSIGNED NOT NULL,
     `title`   VARCHAR(255) NOT NULL,
     `content` MEDIUMTEXT   NOT NULL,
-    `image`   VARCHAR(255) NOT NULL,
+    `image`   VARCHAR(500) NOT NULL,
     `created` DATETIME     NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
